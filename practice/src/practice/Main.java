31435e2732dfe08a6efcc6e51167d8ace9f829e2
Alwayshyuk <never5686@naver.com>
2022-02-16 10:34:05 +0900

    class 생성

--- practice/src/practice/Main.java
+++ practice/src/practice/Main.java
@@ -1,32 +1,48 @@
 package practice;
 
-import java.util.ArrayList;
-import java.util.Scanner;
+import java.math.BigInteger;
+import java.util.StringTokenizer;
 
 public class Main {
 
 	public static void main(String[] args) {
-		Scanner s = new Scanner(System.in);
-		
-		int A = s.nextInt();
-		
-
-		ArrayList list = new ArrayList();
-		
-		for(int i = 0; i<A ; i++) {
-			int c = (int)(Math.random()*1000001);
-			int B = (int) (Math.random()*2);
-			if(B == 1) {
-				c=-c;
-			}
-			list.add(c);
-		}
-		
-		list.sort(null);
-		System.out.println(list.get(0) + " " + list.get(A-1));
-		
-		s.close();
-
+		String input = "삼십만삼천백십오";
+		System.out.println(han(input));
+		BigInteger val = new BigInteger("FFFF", 16);
+		System.out.println(BigInteger.TEN);
 	}
+
+	public static long han(String input) {
+		long result = 0;
+		long tmpResult = 0;
+		long num = 0;
+		
+		final String NUM = "영일이삼사오육칠팔구";
+		final String UNIT = "십백천만억조";
+		final long[] UNIT_NUM = {10, 100, 1000, 10000, (long)1e8, (long)1e12};
+		
+		StringTokenizer st = new StringTokenizer(input, UNIT, true);
+		
+		while(st.hasMoreTokens()) {
+			String token = st.nextToken();
+			int check = NUM.indexOf(token);
+		
+			if(check == -1) {
+				if("만억조".indexOf(token) == -1) {
+					tmpResult += (num != 0 ? num : 1) * UNIT_NUM[UNIT.indexOf(token)];
+					} else {
+						tmpResult +=num;
+						result = (tmpResult != 0? tmpResult : 1)*UNIT_NUM[UNIT.indexOf(token)];
+						tmpResult = 0;
+					}
+				num =0;
+			} else {	//숫자인 경우
+				num = check;
+			}
+		}
+		return result + num + tmpResult;
+}
+		
+	
 
 }
