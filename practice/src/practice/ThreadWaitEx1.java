890c1f1db4ac3c417e37923e057d6996807ffd5a
Alwayshyuk <never5686@naver.com>
2022-03-17 23:17:19 +0900

    예제풀이

---
+++ practice/src/practice/ThreadWaitEx1.java
@@ -0,0 +1,97 @@
+package practice;
+
+import java.util.ArrayList;
+
+public class ThreadWaitEx1 {
+
+	public static void main(String[] args) throws Exception{
+		Table table = new Table();	//여러 쓰레드가 공유하는 객체
+		
+		new Thread(new Cook(table), "COOK1").start();
+		new Thread(new Customer(table, "donut"), "CUST1").start();
+		new Thread(new Customer(table, "burger"), "CUST2").start();
+		
+		Thread.sleep(2000);	
+		System.exit(0);
+	}
+}
+class Customer implements Runnable	{
+	private Table table;
+	private String food;
+	
+	Customer (Table table, String food){
+		this.table = table;
+		this.food = food;
+	}
+	public void run() {
+		while(true) {
+			try {Thread.sleep(100);}catch(InterruptedException e) {}
+			String name = Thread.currentThread().getName();
+			
+			table.remove(food);
+			System.out.println(name+ " ate a "+food);
+		}
+	}
+}
+class Cook implements Runnable	{
+	private Table table;
+	
+	Cook(Table table){this.table = table;}
+	
+	public void run() {
+		while(true) {
+			//임의의 요리를 하나 선택해서 table에 추가한다.
+			int idx = (int)(Math.random()*table.dishNum());
+			table.add(table.dishNames[idx]);
+			try {Thread.sleep(10);}catch(InterruptedException e) {}
+		}
+	}
+}
+class Table{
+	String[] dishNames = {"donut", "donut", "burger"};//donut이 더 자주 나온다.
+	final int MAX_FOOD = 6;//테이블에 놓을 수 있는 최대 음식의 개수
+	
+	private ArrayList<String> dishes = new ArrayList<>();
+	
+	public synchronized void add(String dish) {
+		while(dishes.size()>=MAX_FOOD) {
+			String name = Thread.currentThread().getName();
+			System.out.println(name+" is waiting. ");
+			try {
+				wait();	//cook쓰레드를 기다리게 한다.
+				Thread.sleep(500);
+			} catch (InterruptedException e) {}
+		}
+		dishes.add(dish);
+		notify();	//기다리고 있는 CUST를 깨우기 위함
+		System.out.println("Dish: "+dishes.toString());
+	}
+	public void remove(String dishName) {
+		synchronized(this) {
+			String name = Thread.currentThread().getName();
+			
+			while(dishes.size() == 0) {
+				System.out.println(name + " is waiting. ");
+				try {
+					wait();	//CUST를 기다리게 한다.
+					Thread.sleep(500);
+				}catch(InterruptedException e) {}
+			}
+			while(true) {
+				for(int i = 0; i<dishes.size(); i++) {
+					if(dishName.equals(dishes.get(i))) {
+						dishes.remove(i);
+						notify();	//잠자고 있는 COOK을 깨우기 위함
+						return;
+					}
+				}
+				try {
+					System.out.println(name + " is waiting. ");
+					wait();	//원하는 음식이 없는 CUST쓰레드를 기다리게 한다.
+					Thread.sleep(500);
+				}catch(InterruptedException e) {}
+			}
+		}
+	}
+	public int dishNum() {return dishNames.length;}
+}
