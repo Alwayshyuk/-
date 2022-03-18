43365e8c9edadcf492ec99fef4fcc1483fd7dce0
Alwayshyuk <never5686@naver.com>
2022-03-18 12:04:43 +0900

    예제 풀이

--- practice/src/practice/ThreadWaitEx1.java
+++ practice/src/practice/ThreadWaitEx1.java
@@ -1,6 +1,8 @@
 package practice;
 
 import java.util.ArrayList;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.ReentrantLock;
 
 public class ThreadWaitEx1 {
 
@@ -50,47 +52,61 @@ class Cook implements Runnable	{
 class Table{
 	String[] dishNames = {"donut", "donut", "burger"};//donut이 더 자주 나온다.
 	final int MAX_FOOD = 6;//테이블에 놓을 수 있는 최대 음식의 개수
-	
 	private ArrayList<String> dishes = new ArrayList<>();
 	
-	public synchronized void add(String dish) {
-		while(dishes.size()>=MAX_FOOD) {
-			String name = Thread.currentThread().getName();
-			System.out.println(name+" is waiting. ");
-			try {
-				wait();	//cook쓰레드를 기다리게 한다.
-				Thread.sleep(500);
-			} catch (InterruptedException e) {}
+	private ReentrantLock lock = new ReentrantLock();
+	private Condition forCook = lock.newCondition();
+	private Condition forCust = lock.newCondition();
+	
+	public void add(String dish) {
+		lock.lock();
+		
+		try {
+			while(dishes.size()>=MAX_FOOD) {
+				String name = Thread.currentThread().getName();
+				System.out.println(name+" is waiting. ");
+				
+				try {
+					forCook.await();//wait(); COOK쓰레드를 기다리게 한다.
+					Thread.sleep(500);
+				} catch (InterruptedException e) {}
+			}
+			
+			dishes.add(dish);
+			forCust.signal();//notify();	기다리고 있는 CUST를 깨우기 위함.
+			System.out.println("Dishes: "+ dishes.toString());
+		} finally {
+			lock.unlock();
 		}
-		dishes.add(dish);
-		notify();	//기다리고 있는 CUST를 깨우기 위함
-		System.out.println("Dish: "+dishes.toString());
 	}
 	public void remove(String dishName) {
-		synchronized(this) {
-			String name = Thread.currentThread().getName();
-			
+		lock.lock();
+		String name = Thread.currentThread().getName();
+		
+		try {
 			while(dishes.size() == 0) {
 				System.out.println(name + " is waiting. ");
 				try {
-					wait();	//CUST를 기다리게 한다.
+					forCust.await();//wait();	CUST쓰레드를 기다리게 한다.
 					Thread.sleep(500);
-				}catch(InterruptedException e) {}
+				} catch (InterruptedException e) {}
 			}
 			while(true) {
-				for(int i = 0; i<dishes.size(); i++) {
+				for(int i = 0 ; i<dishes.size(); i++) {
 					if(dishName.equals(dishes.get(i))) {
 						dishes.remove(i);
-						notify();	//잠자고 있는 COOK을 깨우기 위함
+						forCook.signal();//notify();	잠자고 있는 COOK을 깨움
 						return;
 					}
 				}
 				try {
-					System.out.println(name + " is waiting. ");
-					wait();	//원하는 음식이 없는 CUST쓰레드를 기다리게 한다.
+					System.out.println(name + " is waiting.");
+					forCust.await();//wait();	CUST쓰레드를 기다리게 한다.
 					Thread.sleep(500);
-				}catch(InterruptedException e) {}
+				} catch (InterruptedException e) {}
 			}
+		} finally {
+			lock.unlock();
 		}
 	}
 	public int dishNum() {return dishNames.length;}
