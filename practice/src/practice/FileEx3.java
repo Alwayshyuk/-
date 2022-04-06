2f8987186dd0e05a2d495308bd7bfc7c8bcc0b2e
Alwayshyuk <never5686@naver.com>
2022-04-07 01:09:05 +0900

    위치 수정

--- practice/src/practice/FileEx3.java
+++ practice/src/practice/FileEx3.java
@@ -1,4 +1,4 @@
-package tmp;
+package practice;
 
 import java.io.File;
 import java.util.ArrayList;
--- practice/src/practice/FileEx4.java
+++ practice/src/practice/FileEx4.java
@@ -1,4 +1,4 @@
-package tmp;
+package practice;
 
 import java.io.File;
 import java.text.SimpleDateFormat;
